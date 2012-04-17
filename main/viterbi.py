d0dd3d6a187824ddf0a4a96d3c8eb92324282c07
Zach Silversmith <zbs7@cornell.edu>
2012-04-17 00:33:23 -0400

    testes

--- main/viterbi.py
+++ main/viterbi.py
@@ -5,6 +5,9 @@ Created on Apr 10, 2012
 '''
 import HMM
 import cProfile
+from math import log
+
+SMALL_NUM = 1. ** -100
 """
     TODO:
         - Only use the tags encountered in training, and use smoothing/unknown word 
@@ -49,29 +52,38 @@ def viterbi(hmm, filename=None, text=None):
     assert(tokens[0] == '<s>')
     tokens = tokens[1:]
     for tag in TAGS:
-        viterbi[tag] = [hmm.getPriorProbability(('<s>', tag)) * \
-                            hmm.getLikelihoodProbability(tokens[0], tag)]
+        viterbi[tag] = [log(hmm.getPriorProbability(('<s>', tag)) + SMALL_NUM) + \
+                            log(hmm.getLikelihoodProbability(tokens[0], tag)+SMALL_NUM)]
         backpointer[tag] = ['<s>']
     
     # recursion step
     timestep = 0
     for token in tokens[1:]:
         timestep += 1
-        if timestep >= 123:
+        if timestep >= 123 and token == 'III':
             pass
         for tag in TAGS:
+            if timestep >= 123 and token == 'III' and tag == 'NNP':
+                pass    
             # to address issue where 0% likelihood for a given word destroys
             # the rest of the tag sequence
-            best_tag = max(TAGS, key=lambda t: viterbi[t][timestep-1] * \
-                        hmm.getPriorProbability((t, tag)))
+            best_tag = max(TAGS, key=lambda t: viterbi[t][timestep-1] + \
+                        log(hmm.getPriorProbability((t, tag))+SMALL_NUM))
             
             if not hmm.isInVocabulary(token):
-                best_tag_prob = viterbi[best_tag][timestep-1] * \
-                            hmm.getPriorProbability((best_tag, tag))
+                vit = viterbi[best_tag][timestep-1]
+                pri = log(hmm.getPriorProbability((best_tag, tag))+ SMALL_NUM)
+                best_tag_prob = vit + pri
+#                best_tag_prob = viterbi[best_tag][timestep-1] * \
+#                            hmm.getPriorProbability((best_tag, tag))
             else:
-                best_tag_prob = viterbi[best_tag][timestep-1] * \
-                                hmm.getPriorProbability((best_tag, tag)) * \
-                                hmm.getLikelihoodProbability(token, tag)
+                vit = viterbi[best_tag][timestep-1]
+                pri = log(hmm.getPriorProbability((best_tag, tag)) + SMALL_NUM)
+                like = log(hmm.getLikelihoodProbability(token, tag) + SMALL_NUM)
+                best_tag_prob = vit + pri + like
+#                best_tag_prob = viterbi[best_tag][timestep-1] * \
+#                                hmm.getPriorProbability((best_tag, tag)) * \
+#                                hmm.getLikelihoodProbability(token, tag)
                         
             backpointer[tag].append(best_tag)
             viterbi[tag].append(best_tag_prob)
