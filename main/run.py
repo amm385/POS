48729ab66858585e4cdca64fdb29dfb9e54e6be0
Areeb <areeb.malik91@gmail.com>
2012-04-17 14:01:00 -0400

    updated analyzer to fully handle test data

--- main/run.py
+++ main/run.py
@@ -2,19 +2,16 @@ from Analyzer import Analyzer
 
 train_filename = "../train.pos"
 test_filename = "../test-obs.pos"
+test_answers = "../POS solution.txt"
 
-isTest = False #false to use CV, true to use test file
+isTest = True #false to use CV, true to use test file
 
-def get_scores(predicted, actual):
-    tp, fp, fn, tn = 0., 0., 0., 0.
+def get_score(predicted, actual):
+    score = 0.0
     for (p,a) in zip(predicted, actual):
-        tn += len(tags) - 2
         if p == a:
-           tp += 1.
-           tn += 1.
-        else:
-           fp += 1.
-           fn += 1.
+            score += 1.0
+    return score/len(predicted)
     #http://en.wikipedia.org/wiki/Precision_and_recall
     precision = tp / (tp + fp)
     recall = tp / (tp + fn)
@@ -24,22 +21,10 @@ def get_scores(predicted, actual):
     return f_measure, accuracy  
     
 def main():
-    analyzer = Analyzer(isTest,train_filename,test_filename)
-    predicted, actual = analyzer.run()
-    
-    print str(float(len(filter(lambda x: x[0] == x[1], zip(predicted,actual)))) \
-              / float(len(actual)))
-    
-    '''(predicted, actual) = analyzer.run()
-    #(f_measure, accuracy) = get_scores(predicted,actual)
-    #print "F1 Score: " + str(f_measure)
-    #print "Accuracy: " + str(accuracy)
-    score = 0.0
-    for i in range(len(predicted)):
-        if predicted[i] == actual[i]:
-            score += 1.0
-    print "Accuracy: " + str(score/len(predicted))
-	'''
+    analyzer = Analyzer(isTest,train_filename,test_filename,test_answers)
+    (predicted, actual) = analyzer.run()
+    accuracy = get_score(predicted,actual)
+    print "Accuracy: " + str(accuracy)
 
 if __name__ == '__main__':
     main()
